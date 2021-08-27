a55d832db2bee820fbb20603293a731e61067216
Brandon Ullman <ullman.brandon@gmail.com>
2021-08-26 20:34:34 -0700

    added nav bar functionality. Widget page. Counter Widget.

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 function menuButton(x) {
     x.classList.toggle('change');
+    document.querySelector('nav').classList.toggle('change');
+    document.querySelector('nav-buttons').classList.toggle('change');
 }
+
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.button');
+
+btns.forEach(function (button) {
+    button.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else if (styles.contains('reset')) {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        if (count === 0) {
+            value.style.color = 'black';
+        }
+        value.textContent = count;
+    })
+})
